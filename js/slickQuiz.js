68a21c90291bb3f87da3a40865cc8c3ddfb0cd63
Tom Blankenship <tom@novologic.com>
2013-04-29 10:49:32 -0400

    Fix problem with matching answers containing HTML entities.
    At line 367, decodes HTML entities in trueAnswers so the comparison
    against selectedAnswers will work correctly when the original answer
    contains HTML entities.
    
        To duplicate problem:
    
        Create a true answer that contains HTML entities such as this:
    
        {"option": "This is the correct &ldquo;answer&rdquo;.", "correct":
    true}
    
        Run the quiz and select the correct answer and it will return
    incorrect when it should return correct.

--- js/slickQuiz.js
+++ js/slickQuiz.js
@@ -364,7 +364,8 @@
                     selectedAnswers = selectedAnswers.sort();
 
                 for (var i = 0, l = trueAnswers.length; i < l; i++) {
-                    if (trueAnswers[i] !== selectedAnswers[i]) {
+                    var encodedAnswer = $('<div />').html(trueAnswers[i]).text();
+                    if (encodedAnswer !== selectedAnswers[i]) {
                         return false;
                     }
                 }
